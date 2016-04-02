0b29529079f1763ce2de4003d788e94d3d5d4d14
Tony Perez <tonyperez716@gmail.com>
2016-04-02 14:29:48 -0400

    this is a test with the updated comment

--- gitExample/ViewController.swift
+++ gitExample/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         //new comment
+        //new comment 2
     }
 
     override func didReceiveMemoryWarning() {
