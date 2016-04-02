55e06d206862ae72276a11e3e24471d6a7c5a042
Tony Perez <tonyperez716@gmail.com>
2016-04-02 14:25:08 -0400

    this is a test

---
+++ gitExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitExample
+//
+//  Created by Tony Perez on 4/2/16.
+//  Copyright © 2016 Tony Perez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
